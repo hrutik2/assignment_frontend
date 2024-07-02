7f0c722a792dacc37d41383547905c8130e56b19
Rutik Jadhav <112790437+hrutik2@users.noreply.github.com>
2024-07-02 16:15:24 +0530

    Update Filter.js

--- my-react-app/src/component/Filter.js
+++ my-react-app/src/component/Filter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Select from 'react-select';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
@@ -8,26 +7,24 @@ const StyledButton = styled(Button)`
   border-radius: 10px;
   border: 1px solid black;
   color:black
-`;
-
+`
 
 const Filter = ({ setFilter }) => {
-  const handleChange = (selectedOption) => {
-    setFilter(selectedOption.value);
+  const handleFilterClick = (filterValue) => {
+    setFilter(filterValue);
   };
 
   return (
-    <div style={{margin:"10px"}}>
+    <div style={{ margin: "10px" }}>
       <Stack spacing={1} direction="row">
-      
-      <StyledButton variant="outlined" size="small">All</StyledButton>
-      <StyledButton variant="outlined" size="small">XHR/Fetch</StyledButton>
-      <StyledButton variant="outlined" size="small">JS</StyledButton>
-      <StyledButton variant="outlined" size="small">CSS</StyledButton>
-    </Stack>
-      
+        <StyledButton variant="outlined" size="small" onClick={() => handleFilterClick('All')}>All</StyledButton>
+        <StyledButton variant="outlined" size="small" onClick={()  => handleFilterClick('XHR/Fetch')}>XHR/Fetch</StyledButton>
+        <StyledButton variant="outlined" size="small" onClick={() => handleFilterClick('JS')}>JS</StyledButton>
+        <StyledButton variant="outlined" size="small" onClick={() => handleFilterClick('CSS')}>CSS</StyledButton>
+      </Stack>
     </div>
   );
 };
 
 export default Filter;
+
